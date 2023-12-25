09534911c7b2fe5a348bb3aa29cbf1b1374d11f6
Stefan Gatu <stefan.gatu@gmail.com>
2023-12-25 22:09:40 +0000

    feat: redis repository to save and retrieve game state

---
+++ infrastructure/repositories/gameRepository.go
@@ -0,0 +1,100 @@
+package repositories
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/json"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/sgatu/chezz-back/game"
+	"github.com/sgatu/chezz-back/models"
+)
+
+type gameMarshalStruct struct {
+	GameState   []byte
+	WhitePlayer int64
+	BlackPlayer int64
+	GameId      int64
+}
+
+type RedisGameRepository struct {
+	redisConn *redis.Client
+	opTimeout int
+	ctx       context.Context
+}
+
+func NewRedisGameRepository(host string, port int, opTimeout int) models.GameRepository {
+	return &RedisGameRepository{
+		redisConn: redis.NewClient(&redis.Options{
+			Addr:     fmt.Sprintf("%s:%d", host, port),
+			Password: "",
+		}),
+		opTimeout: opTimeout,
+		ctx:       context.Background(),
+	}
+}
+
+func (rgr *RedisGameRepository) GetGame(id int64) (*models.Game, error) {
+	cmdResult := rgr.redisConn.Get(rgr.ctx, rgr.getGameKey(id))
+	result, err := cmdResult.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	return rgr.recoverGame(result)
+}
+
+func (rgr *RedisGameRepository) SaveGame(g *models.Game) error {
+	gameSerialized, err := rgr.serializeGame(g)
+	if err != nil {
+		return err
+	}
+	status := rgr.redisConn.Set(rgr.ctx, rgr.getGameKey(g.Id()), gameSerialized, time.Hour*time.Duration(24))
+	if status.Err() != nil {
+		return status.Err()
+	}
+	return nil
+}
+
+func (rgr *RedisGameRepository) getGameKey(id int64) string {
+	rawId := [8]byte{}
+	binary.LittleEndian.PutUint64(rawId[:], uint64(id))
+	return "game.{" + base64.RawStdEncoding.EncodeToString(rawId[:]) + "}"
+}
+
+func (rgr *RedisGameRepository) serializeGame(g *models.Game) ([]byte, error) {
+	if g == nil {
+		return nil, fmt.Errorf("game is nil")
+	}
+	gameStatusSerialized, err := g.GameState().Serialize()
+	if err != nil {
+		return []byte{}, fmt.Errorf("cannot serialize game")
+	}
+	return json.Marshal(&gameMarshalStruct{
+		GameState:   gameStatusSerialized,
+		WhitePlayer: g.WhitePlayer(),
+		BlackPlayer: g.BlackPlayer(),
+		GameId:      g.Id(),
+	})
+}
+
+func (rgr *RedisGameRepository) recoverGame(data []byte) (*models.Game, error) {
+	unmarshaledData := &gameMarshalStruct{}
+	err := json.Unmarshal(data, unmarshaledData)
+	if err != nil {
+		return nil, err
+	}
+	gameState, err := game.FromSerialized(unmarshaledData.GameState)
+	if err != nil {
+		return nil, err
+	}
+	return models.RecoverGameState(
+			unmarshaledData.GameId,
+			unmarshaledData.WhitePlayer,
+			unmarshaledData.BlackPlayer,
+			gameState),
+		nil
+}
